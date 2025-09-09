14b103fe1c209dcb5bc48b27d1773d06bd3cc0c5
Thorsten Klein <tk@thklein.io>
2025-09-09 11:31:06 +0200

    fix: special addressbooks

--- config.inc.php
+++ config.inc.php
@@ -11,34 +11,25 @@
 ///////////////////////////////////////////////////////////////////////
 
 
-//// ** GLOBAL SETTINGS
-
-// Disallow users to add custom addressbooks (default: false)
-// $prefs['_GLOBAL']['fixed'] = true;
-
-// When enabled, this option hides the 'CardDAV' section inside Preferences.
-// $prefs['_GLOBAL']['hide_preferences'] = true;
-
 // Scheme for storing the CardDAV passwords, in order from least to best security.
 // Options: plain, base64, des_key, encrypted (default)
 // $prefs['_GLOBAL']['pwstore_scheme'] = 'encrypted';
+$prefs['_GLOBAL']['pwstore_scheme']  = 'encrypted';
 
 // Specify minimum loglevels for logging for the plugin and the HTTP client
 // The following are possible: DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL, ALERT, EMERGENCY
 // Defaults to ERROR
-$prefs['_GLOBAL']['loglevel'] = \Psr\Log\LogLevel::WARNING;
-$prefs['_GLOBAL']['loglevel_http'] = \Psr\Log\LogLevel::ERROR;
-
-// --- Global ---
-$prefs['_GLOBAL']['pwstore_scheme']  = 'encrypted';
 $prefs['_GLOBAL']['loglevel']        = \Psr\Log\LogLevel::WARNING;
 $prefs['_GLOBAL']['loglevel_http']   = \Psr\Log\LogLevel::ERROR;
 
-// UI offen lassen, damit die User ihr Nextcloud-App-Passwort eintragen können
+// Disallow users to add custom addressbooks (default: false)
 $prefs['_GLOBAL']['fixed']            = true;
+
+// When enabled, this option hides the 'CardDAV' section inside Preferences.
 $prefs['_GLOBAL']['hide_preferences'] = true;
 
-// --- Persönliche Nextcloud-Kontakte (pro User, ohne Passwort -> User trägt App-Token ein) ---
+
+// ***** Nextcloud CardDAV Connection *****
 $prefs['nextcloud_user'] = [
     'accountname'          => 'Souvera',
     //'username'             => '%u',   // falls OIDC-Name != NC-Name, kann der User es im UI anpassen
@@ -57,27 +48,20 @@ $prefs['nextcloud_user'] = [
     'fixed'        =>  ['discovery_url', 'username', 'password', 'ssl_noverify', 'preemptive_basic_auth'],
 ];
 
-// --- Mappings: Roundcube-Rollen -> konkrete Bücher beim jeweiligen Nutzer ---
-// Persönliches Standard-Adressbuch -> .../users/%u/kontakte/
+//***** Override Roundcube special addressbooks ***** 
 $prefs['_GLOBAL']['default_addressbook'] = [
     'preset'    => 'nextcloud_user',
-    'matchurl' => '#https://%HOST%/remote.php/addressbooks/users/%u/contacts#',
-    // alternativ/zusätzlich möglich:
-//    'matchname' => '/^Souvera: Contacts$/i',
+    'matchurl' => '#/contacts/?$#',
 ];
 
-
-
-// Gesammelte Empfänger -> .../users/%u/z-app-generated--contactsinteraction--recent/
 $prefs['_GLOBAL']['collected_recipients'] = [
-    'preset'    => 'nextcloud_user',
-    'matchurl'  => '#https://%HOST%/remote.php/dav/addressbooks/users/%u/z-app-generated--contactsinteraction--recent/#',
+	'preset'    => 'nextcloud_user',
+    'matchurl'  => '#/z-app-generated--contactsinteraction--recent/?$#',
 ];
 
 $prefs['_GLOBAL']['collected_senders'] = [
     'preset'    => 'nextcloud_user',
-    'matchurl'  => '#https://%HOST%/remote.php/dav/addressbooks/users/%u/z-app-generated--contactsinteraction--recent/#',
+    'matchurl'  => '#/z-app-generated--contactsinteraction--recent/?$#',
 ];
-
 
 // vim: ts=4:sw=4:expandtab:fenc=utf8:ff=unix:tw=120:ft=php
